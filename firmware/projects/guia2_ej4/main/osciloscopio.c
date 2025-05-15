3ddd084884a8f0665127fb778d418a0bc83af5c9
GradibusApp <francisco.rode@ingenieria.uner.edu.ar>
2025-05-15 10:59:57 -0300

    Subo parte del ECG ejercicio 4 guia 2 para flashear en PC Lab.

--- firmware/projects/guia2_ej4/main/osciloscopio.c
+++ firmware/projects/guia2_ej4/main/osciloscopio.c
@@ -34,6 +34,9 @@
 #include "uart_mcu.h"
 /*==================[macros and definitions]=================================*/
 
+/** @brief Tamano de buffer ECG */
+#define BUFFER_SIZE 231
+
 /** @brief Frecuencia de muestreo de señal analógica */
 #define FRECUENCIA_MUESTREO 500
 
@@ -42,8 +45,41 @@
 /** @brief Handler que maneja la tarea asociada al muestreo */
 TaskHandle_t handlerMuestreo;
 
+/** @brief Handler que maneja la tarea asociada al muestreo */
+TaskHandle_t handlerEnvioECG;
+
 /** @brief Voltaje medido */
 uint16_t voltaje = 0;
+
+/** @brief Periodo de interrupcion del timer de muestreo */
+uint16_t periodoTimerMuestreo = (1/FRECUENCIA_MUESTREO)*1000000;
+
+/** @brief Periodo de interrupcion del timer de envio*/
+uint16_t periodoTimerEnvio = periodoTimerMuestreo/2;
+
+/** @brief Muestras de ECG */
+const char ecg[BUFFER_SIZE] = {
+    76, 77, 78, 77, 79, 86, 81, 76, 84, 93, 85, 80,
+    89, 95, 89, 85, 93, 98, 94, 88, 98, 105, 96, 91,
+    99, 105, 101, 96, 102, 106, 101, 96, 100, 107, 101,
+    94, 100, 104, 100, 91, 99, 103, 98, 91, 96, 105, 95,
+    88, 95, 100, 94, 85, 93, 99, 92, 84, 91, 96, 87, 80,
+    83, 92, 86, 78, 84, 89, 79, 73, 81, 83, 78, 70, 80, 82,
+    79, 69, 80, 82, 81, 70, 75, 81, 77, 74, 79, 83, 82, 72,
+    80, 87, 79, 76, 85, 95, 87, 81, 88, 93, 88, 84, 87, 94,
+    86, 82, 85, 94, 85, 82, 85, 95, 86, 83, 92, 99, 91, 88,
+    94, 98, 95, 90, 97, 105, 104, 94, 98, 114, 117, 124, 144,
+    180, 210, 236, 253, 227, 171, 99, 49, 34, 29, 43, 69, 89,
+    89, 90, 98, 107, 104, 98, 104, 110, 102, 98, 103, 111, 101,
+    94, 103, 108, 102, 95, 97, 106, 100, 92, 101, 103, 100, 94, 98,
+    103, 96, 90, 98, 103, 97, 90, 99, 104, 95, 90, 99, 104, 100, 93,
+    100, 106, 101, 93, 101, 105, 103, 96, 105, 112, 105, 99, 103, 108,
+    99, 96, 102, 106, 99, 90, 92, 100, 87, 80, 82, 88, 77, 69, 75, 79,
+    74, 67, 71, 78, 72, 67, 73, 81, 77, 71, 75, 84, 79, 77, 77, 76, 76,
+};
+
+/** @brief Indice que intera sobre las muestras del ecg */
+uint16_t indiceECG = 0;
 
 
 /*==================[internal functions declaration]=========================*/
@@ -54,6 +90,13 @@ uint16_t voltaje = 0;
 void FuncTimerA(void* param)
 {
 	vTaskNotifyGiveFromISR(handlerMuestreo, pdFALSE); 
+}
+/**
+ * @brief Función que maneja la interrupción del timer B
+ */
+void FuncTimerB(void* param)
+{
+	vTaskNotifyGiveFromISR(enviar_task_handle, pdFALSE);
 }
 /**
  * @brief Tarea que muestrea la señal que ingresa en el canal 1
@@ -72,6 +115,24 @@ static void muestrearTask(void *pvParameter){
 		UartSendString(UART_PC, " \r\n");
 	}
 }
+/**
+ * @brief Tarea que envia la conversion analogica al canal 0.
+ *
+ * @param pvParameter parámetro utilizado por freeRTOS
+ */
+
+static void enviarTask(void *pvParameter){
+
+	while(true)
+	{
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		if (indiceECG == BUFFER_SIZE-1) {
+			indiceECG = 0;
+		}
+		AnalogOutputWrite(ecg[indiceECG]);
+		indiceECG++;
+	}
+}
 
 
 /*==================[external functions definition]==========================*/
@@ -79,12 +140,17 @@ void app_main(void){
 
 	timer_config_t timerMuestreo = {
         .timer = TIMER_A,
-        .period = 2000,
+        .period = periodoTimerMuestreo,
         .func_p = FuncTimerA,
         .param_p = NULL
     };
+	timer_config_t timerEnvio = {
+        .timer = TIMER_B,
+        .period = periodoTimerEnvio,
+        .func_p = FuncTimerB,
+        .param_p = NULL
+    };
 	
-
 	analog_input_config_t configAnalog = {
 		.input = CH1,
 		.mode = ADC_SINGLE,
@@ -103,11 +169,15 @@ void app_main(void){
 	
 
 	TimerInit(&timerMuestreo); 
-	UartInit(&puertoSerie);
-	AnalogInputInit(&configAnalog);
-	//AnalogOutputInit();
-	xTaskCreate(&muestrearTask, "taskMuestrear", 512, NULL, 5, &handlerMuestreo);
+	TimerInit(&timerEnvio);
 	TimerStart(TIMER_A);
+	TimerStart(TIMER_B);
+	AnalogInputInit(&configAnalog);
+	AnalogOutputInit();
+	xTaskCreate(&muestrearTask, "taskMuestrear", 512, NULL, 5, &handlerMuestreo);
+	xTaskCreate(&enviarTask, "taskEnvioECG", 2048, NULL, 5, &handlerEnvioECG);
+	UartInit(&puertoSerie);
+	
 
 }
 /*==================[end of file]============================================*/
