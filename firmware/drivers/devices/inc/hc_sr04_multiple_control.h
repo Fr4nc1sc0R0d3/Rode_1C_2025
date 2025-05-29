6bd9cf377c7b4067836476f6ad2fc7970ccca241
Fr4nc1sc0R0d3 <francisco.rode@ingenieria.uner.edu.ar>
2025-05-29 09:21:09 -0300

    subo primer version de proyecto final, y archivos que quedaron colgados de subir

---
+++ firmware/drivers/devices/inc/hc_sr04_multiple_control.h
@@ -0,0 +1,22 @@
+#ifndef HC_SR04_MULTIPLE_CONTROL_H
+#define HC_SR04_MULTIPLE_CONTROL_H
+
+#include "gpio_mcu.h"
+#include "hc_sr04.h"
+
+typedef struct {
+    gpio_t echo;
+    gpio_t trigger;
+} HCSR04;
+
+typedef struct {
+    uint16_t s1;
+    uint16_t s2;
+    uint16_t s3;
+    uint16_t s4;
+} medicionesHCSRO4;
+
+
+uint16_t obtenerMediciones(HCSR04 *vectorGPIOs);
+
+#endif
---
+++ firmware/drivers/devices/src/hc_sr04_multiple_control.c
@@ -0,0 +1,20 @@
+#include "hc_sr04_multiple_control.h"
+
+
+medicionesHCSRO4 obtenerMediciones(HCSR04 *vectorGPIOs) {
+    medicionesHCSRO4 mediciones = {0};
+
+    for (uint8_t i = 0; i < 4; i++) {
+        HcSr04Init(vectorGPIOs[i].echo, vectorGPIOs[i].trigger);
+        uint16_t distancia = HcSr04ReadDistanceInCentimeters();
+        switch (i) {
+            case 0: mediciones.s1 = distancia; break;
+            case 1: mediciones.s2 = distancia; break;
+            case 2: mediciones.s3 = distancia; break;
+            case 3: mediciones.s4 = distancia; break;
+        }
+    }
+
+    return mediciones;
+}
+
